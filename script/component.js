58ff84e32011e1ffd6a21728dc8664de098648ea
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-01-31 14:45:23 +0000

    vidas extra

--- script/component.js
+++ script/component.js
@@ -135,3 +135,35 @@ class Bullets extends Component{
     }
 }
 
+class Heart extends Component{
+    constructor(x,y,w,h,ctx, heartSpeed) {
+        super(x,y,w,h,ctx)
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+            this.ctx = ctx;  
+            this.image = new Image(); 
+            this.speedX = heartSpeed;
+    }
+    draw(){
+        this.image.src="../images/heart.png";
+        ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
+    }
+    newPos(){
+        this.x += this.speedX;
+    } 
+    top (){
+        return this.y;
+    }
+    bottom (){
+        return this.y + this.h;
+    }
+    left(){
+        return this.x;
+    }
+    right(){
+        return this.x + this.w;
+    }
+
+}
